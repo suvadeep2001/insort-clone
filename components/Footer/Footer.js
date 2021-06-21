6c9eeeb78fce68863513a8e67294f5fd51676b48
Suvadeep Maity <68159874+AndrewSuv@users.noreply.github.com>
2021-06-22 00:34:59 +0530

    Add files via upload

---
+++ components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="name">
+        Inshorts clone made by -{" "}
+        <a href="https://www.linkedin.com/in/suvadeep-maity-392a881a7/" target="__blank">
+          Suvadeep Maity
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ data/category.js
@@ -0,0 +1,13 @@
+const categories = [
+
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+ 
+];
+
+export default categories;
